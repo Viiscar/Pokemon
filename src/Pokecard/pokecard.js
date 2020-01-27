f8a83313ebfae4e43cb17f6ca2bf9489fc639ca7
FLR <fanchleroux@tutanota.com>
2020-01-27 15:50:51 -0400

    Adding css flexbox

---
+++ src/Pokecard/pokecard.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+function Pokecard(props) { 
+
+    const[pokemon, setPokemon] = useState(props.pokelist)
+    const [sprite, setSprite] = useState();
+
+    //récupérer les sprites des pokémons
+    useEffect(() => {
+     
+      setPokemon(props.pokelist)
+      const fetchData2 = async () => {
+            
+        const fetchResult = await fetch(props.url)
+          .then(response => response.json());
+        
+        setSprite(fetchResult.sprites.front_default);
+
+      }
+      
+      fetchData2();
+    }, [pokemon]);
+    console.log(sprite);
+    const pokemonDetails = pokemon ? pokemon : {name : ''};
+    return (
+
+      <div class="pokecard"> 
+        <label class="pokename">
+            {pokemonDetails.name}
+        </label>
+        <img src={sprite} alt={pokemonDetails.name}></img>
+      </div>
+    )}
+
+export default Pokecard;
